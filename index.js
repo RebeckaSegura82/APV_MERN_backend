2e899c03e9eeb002b3c1b50a9dff33b42e043124
Rebecka Segura <asesoriashoy0@gmail.com>
2022-09-12 20:42:53 -0500

    corchetes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 conectarDB();
 
 
-const dominiosPermitidos = [process.env.FRONTEND_URL];
+const dominiosPermitidos = process.env.FRONTEND_URL;
 
 const corsOptions = {
     origin: function(origin, callback) {
